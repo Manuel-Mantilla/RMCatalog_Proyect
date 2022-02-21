52bd06f29c938a3db95f605da10d17032376c514
Manuel-Mantilla <santo2244@gmail.com>
2022-02-20 21:05:39 -0500

    Initial commit

---
+++ RMCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RMCatalog
+//
+//  Created by Manuel Mantilla on 20/02/22.
+//  Copyright © 2022 Manuel Mantilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
